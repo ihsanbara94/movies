e50eea645662609f2541c302c189ce0f4b9e61ab
ihsanbara94 <ihsanbatubara94@gmail.com>
2022-05-31 13:34:04 +0700

    road to deploy v1

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <Navigation />
-
     <div class="flex my-0 mx-auto px-0 py-3 max-w-[1140px] justify-center">
       <div class="px-4 block space-y-3 justify-start w-1/4">
         <p>Search</p>
@@ -28,6 +27,7 @@
             type="text"
             placeholder="Input Year"
             class="px-2 py-2 border-solid border-2 w-full"
+            v-model="year"
           />
 
           <p>Genre</p>
@@ -49,6 +49,12 @@
             :isOpen="isOpenFilterSort"
             @click-list="chooseFilterSort"
           />
+          <button
+            class="mx-auto my-5 bg-blue-500 text-white px-4 py-2 rounded font-bold"
+            @click="executeFilter()"
+          >
+            Filter
+          </button>
         </div>
       </div>
       <div class="w-2/3">
@@ -85,16 +91,25 @@ export default {
       Sort: [],
       filter: "E.g. Action ",
       isOpenFilter: false,
-      listDropdownGenre: ["Ascending", "Descending"],
+      listDropdownGenre: [
+        "Action",
+        "Animation",
+        "Comedy",
+        "Documentary",
+        "Horror",
+        "Romance",
+      ],
       filterSort: "E.g. Popularity ",
       isOpenFilterSort: false,
-      listDropdownSort: ["Popularity", "Release Date"],
+      listDropdownSort: ["Popularity", "Release Date", "Revenue", "Most Vote"],
       searchKey: "",
       useFilter: false,
       urlDiscover: "https://api.themoviedb.org/3/discover/movie",
       urlSearch: "https://api.themoviedb.org/3/search/movie",
       sortBy: "popularity.desc",
-      year: "2020",
+      year: "",
+      sortBy: "",
+      filterGenreById: 0,
     };
   },
   watch: {
@@ -108,51 +123,89 @@ export default {
   methods: {
     getDataMovies() {
       axios
-        .get(this.setEndpointParam())
+        .get(
+          this.setEndpointParam(this.sortBy, this.year, this.filterGenreById)
+        )
         .then((res) => {
           this.Movies = res.data.results;
-          console.log(res.data.results);
         })
         .catch((err) => {
           console.log(err);
         });
     },
 
-    setEndpointParam() {
-      // let { sortBy, page, year, filterYear, filterGenreById } = val;
+    setEndpointParam(sortBy, year, filterGenreById) {
       if (!this.useFilter) {
         let url =
           this.searchKey == ""
-            ? `${this.urlDiscover}?api_key=${this.apiKey}&language=en-US&sort_by=${this.sortBy}&page=${this.page}&release_date.gte=${this.year}`
+            ? `${this.urlDiscover}?api_key=${this.apiKey}&page=${this.page}`
             : `${this.urlSearch}?api_key=${this.apiKey}&query=${this.searchKey}&page=${this.page}`;
         return url;
+      } else {
+        let url = `${this.urlDiscover}?api_key=${this.apiKey}&language=en-US&page=${this.page}`;
+        year && (url += `&primary_release_year=${year}`);
+        filterGenreById && (url += `&with_genres=${filterGenreById}`);
+        sortBy && (url += `&sort_by=${sortBy}`);
+        return url;
       }
-      // else {
-      //   let url = `${this.urlDiscover}?api_key=${this.apiKey}&language=en-US&page=${this.page}`
-      //   filterYear && (url += `&primary_release_year=${filterYear}`)
-      //   filterGenreById && (url += `&with_genres=${filterGenreById}`)
-      //   sortBy && (url += `&sort_by=${this.sortBy}`)
-      //   return url
-      // }
     },
 
-    setPage() {
-      this.page += 1;
+    executeFilter() {
+      this.useFilter = true;
+      if (this.year || this.filterGenreById || this.sortBy) {
+        this.Movies.value = [];
+        this.getDataMovies();
+      }
     },
+
     chooseFilter(param) {
       console.log(param);
-      if (param === "Ascending") {
-        this.search = this.search.sort(
-          (a, b) =>
-            parseInt(a.createdAt.split("-").join("")) -
-            parseInt(b.createdAt.split("-").join(""))
-        );
-      } else if (param === "Descending") {
-        this.search = this.search.sort(
-          (a, b) =>
-            parseInt(b.createdAt.split("-").join("")) -
-            parseInt(a.createdAt.split("-").join(""))
-        );
+      if (param === "Action") {
+        this.filterGenreById = 28;
+        this.filter = "Action";
+        this.isOpenFilter = !this.isOpenFilter;
+      } else if (param === "Animation") {
+        this.filterGenreById = 16;
+        this.filter = "Animation";
+        this.isOpenFilter = !this.isOpenFilter;
+      } else if (param === "Comedy") {
+        this.filterGenreById = 35;
+        this.filter = "Comedy";
+        this.isOpenFilter = !this.isOpenFilter;
+      } else if (param === "Documentary") {
+        this.filterGenreById = 99;
+        this.filter = "Documentary";
+        this.isOpenFilter = !this.isOpenFilter;
+      } else if (param === "Horror") {
+        this.filterGenreById = 27;
+        this.filter = "Horror";
+        this.isOpenFilter = !this.isOpenFilter;
+      } else if (param === "Romance") {
+        this.filterGenreById = 10749;
+        this.filter = "Romance";
+        this.isOpenFilter = !this.isOpenFilter;
+      } else {
+        console.log(param);
+      }
+    },
+    chooseFilterSort(param) {
+      console.log(param);
+      if (param === "Popularity") {
+        this.sortBy = "popularity.desc";
+        this.filterSort = "Popularity";
+        this.isOpenFilterSort = !this.isOpenFilterSort;
+      } else if (param === "Release Date") {
+        this.sortBy = "release_date.desc";
+        this.filterSort = "Release Date";
+        this.isOpenFilterSort = !this.isOpenFilterSort;
+      } else if (param === "Revenue") {
+        this.sortBy = "revenue.desc";
+        this.filterSort = "Revenue";
+        this.isOpenFilterSort = !this.isOpenFilterSort;
+      } else if (param === "Most Vote") {
+        this.sortBy = "vote_count.desc";
+        this.filterSort = "Most Vote";
+        this.isOpenFilterSort = !this.isOpenFilterSort;
       } else {
         console.log(param);
       }
@@ -163,12 +216,15 @@ export default {
     dropdownFilterSort() {
       this.isOpenFilterSort = !this.isOpenFilterSort;
     },
-    chooseFilterSort() {
+    chooseFilterSortSort() {
       alert("ihsan");
+    },
+    setPage() {
+      this.page += 1;
     },
     getResult() {
       this.useFilter = false;
-      this.page = 1;
+      // this.page = 1;
       this.Movies = [];
       this.getDataMovies();
     },
